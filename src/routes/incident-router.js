5bcdb4a81ca232c5f66415d1fb742c8ed3f506d7
muthuslm2006 <98971383+muthuslm2006@users.noreply.github.com>
2025-01-02 16:10:35 +0530

    incident-cors

--- src/routes/incident-router.js
+++ src/routes/incident-router.js
@@ -3,7 +3,22 @@ var router = express.Router();
 const { AuthToken } = require('stashook-utils');
 const IncidentController = require('../controller/incident-controller')
 
-router.post('/searchIncident', AuthToken.validateToken, IncidentController.searchIncident);
+const cors =require('cors');
+var whitelist = ['http://localhost:4200']
+let corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+router.options('*',cors());
+
+
+
+router.post('/searchIncident',cors(corsOptionsDelegate), AuthToken.validateToken, IncidentController.searchIncident);
 
 router.post('/getIncident', AuthToken.validateToken, IncidentController.getIncident);
 
